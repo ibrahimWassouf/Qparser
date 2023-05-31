b85eb53ffdf76a6ff5c22552d59c06b7d7609b75
Ibrahim Wassouf <ib981347@dal.ca>
2023-05-30 21:01:31 -0300

    Add node dependencies and implement file reader
    I started setting up the scaffolding of the REPL and have tried creating
    a function that will read the contents of the file passsed to the parser.
    Right now, the REPL just prints out the content but hopefully soon it will
    be able to parse the contents.

--- repl.ts
+++ repl.ts
@@ -1,1 +1,20 @@
-export default function main(args: string[]) {}
+import * as fs from "node:fs/promises";
+import * as process from "process";
+
+export default function main(args: string[]) {
+  if (args.length > 1) {
+    console.log("Usage: jlox [script]");
+  } else if (args.length == 1) {
+    runFile(args[0]);
+  } else {
+    console.log("Where REPL start; to be implemented");
+  }
+}
+
+export async function runFile(path: string) {
+  const textContent = await fs.readFile(path);
+  console.log(textContent.toString());
+}
+
+const args = process.argv.slice(2);
+module.exports.init = main(args);
