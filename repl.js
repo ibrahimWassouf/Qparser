bd476a69486ca4c112fc732fee601d04900fd056
Ibrahim Wassouf <ib981347@dal.ca>
2023-06-09 22:00:11 -0300

    Add pseudo repl functionality
    this will mimick how a repl will behave by allowing
    a string of input and will process the line after entering
    a new line. You can exit out of the repl by entering ':q'

--- repl.js
+++ repl.js
@@ -35,10 +35,18 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
         if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
     }
 };
+var __asyncValues = (this && this.__asyncValues) || function (o) {
+    if (!Symbol.asyncIterator) throw new TypeError("Symbol.asyncIterator is not defined.");
+    var m = o[Symbol.asyncIterator], i;
+    return m ? m.call(o) : (o = typeof __values === "function" ? __values(o) : o[Symbol.iterator](), i = {}, verb("next"), verb("throw"), verb("return"), i[Symbol.asyncIterator] = function () { return this; }, i);
+    function verb(n) { i[n] = o[n] && function (v) { return new Promise(function (resolve, reject) { v = o[n](v), settle(resolve, reject, v.done, v.value); }); }; }
+    function settle(resolve, reject, d, v) { Promise.resolve(v).then(function(v) { resolve({ value: v, done: d }); }, reject); }
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.runFile = void 0;
-var fs = require("node:fs/promises");
+var fsPromise = require("node:fs/promises");
 var process = require("process");
+var readline = require("node:readline/promises");
 function main(args) {
     if (args.length > 1) {
         console.log("Usage: jlox [script]");
@@ -47,7 +55,8 @@ function main(args) {
         runFile(args[0]);
     }
     else {
-        console.log("Where REPL start; to be implemented");
+        //console.log("Where REPL start; to be implemented");
+        runPrompt();
     }
 }
 exports.default = main;
@@ -56,7 +65,7 @@ function runFile(path) {
         var textContent;
         return __generator(this, function (_a) {
             switch (_a.label) {
-                case 0: return [4 /*yield*/, fs.readFile(path)];
+                case 0: return [4 /*yield*/, fsPromise.readFile(path)];
                 case 1:
                     textContent = _a.sent();
                     console.log(textContent.toString());
@@ -66,5 +75,63 @@ function runFile(path) {
     });
 }
 exports.runFile = runFile;
+function runPrompt() {
+    var _a, e_1, _b, _c;
+    return __awaiter(this, void 0, void 0, function () {
+        var rl, _d, rl_1, rl_1_1, line, e_1_1;
+        return __generator(this, function (_e) {
+            switch (_e.label) {
+                case 0:
+                    rl = readline.createInterface({
+                        input: process.stdin,
+                    });
+                    process.stdout.write("> ");
+                    _e.label = 1;
+                case 1:
+                    _e.trys.push([1, 6, 7, 12]);
+                    _d = true, rl_1 = __asyncValues(rl);
+                    _e.label = 2;
+                case 2: return [4 /*yield*/, rl_1.next()];
+                case 3:
+                    if (!(rl_1_1 = _e.sent(), _a = rl_1_1.done, !_a)) return [3 /*break*/, 5];
+                    _c = rl_1_1.value;
+                    _d = false;
+                    try {
+                        line = _c;
+                        if (line === ":q")
+                            return [3 /*break*/, 5];
+                        line.slice(2);
+                        process.stdout.write(line + "\n");
+                        process.stdout.write("> ");
+                    }
+                    finally {
+                        _d = true;
+                    }
+                    _e.label = 4;
+                case 4: return [3 /*break*/, 2];
+                case 5: return [3 /*break*/, 12];
+                case 6:
+                    e_1_1 = _e.sent();
+                    e_1 = { error: e_1_1 };
+                    return [3 /*break*/, 12];
+                case 7:
+                    _e.trys.push([7, , 10, 11]);
+                    if (!(!_d && !_a && (_b = rl_1.return))) return [3 /*break*/, 9];
+                    return [4 /*yield*/, _b.call(rl_1)];
+                case 8:
+                    _e.sent();
+                    _e.label = 9;
+                case 9: return [3 /*break*/, 11];
+                case 10:
+                    if (e_1) throw e_1.error;
+                    return [7 /*endfinally*/];
+                case 11: return [7 /*endfinally*/];
+                case 12:
+                    process.exit();
+                    return [2 /*return*/];
+            }
+        });
+    });
+}
 var args = process.argv.slice(2);
 module.exports.init = main(args);
--- repl.ts
+++ repl.ts
@@ -1,5 +1,6 @@
-import * as fs from "node:fs/promises";
+import * as fsPromise from "node:fs/promises";
 import * as process from "process";
+import * as readline from "node:readline/promises";
 
 export default function main(args: string[]) {
   if (args.length > 1) {
@@ -7,14 +8,29 @@ export default function main(args: string[]) {
   } else if (args.length == 1) {
     runFile(args[0]);
   } else {
-    console.log("Where REPL start; to be implemented");
+    //console.log("Where REPL start; to be implemented");
+    runPrompt();
   }
 }
 
 export async function runFile(path: string) {
-  const textContent = await fs.readFile(path);
+  const textContent = await fsPromise.readFile(path);
   console.log(textContent.toString());
 }
 
+async function runPrompt() {
+  const rl = readline.createInterface({
+    input: process.stdin,
+  });
+
+  process.stdout.write("> ");
+  for await (const line of rl) {
+    if (line === ":q") break;
+    line.slice(2);
+    process.stdout.write(line + "\n");
+    process.stdout.write("> ");
+  }
+  process.exit();
+}
 const args = process.argv.slice(2);
 module.exports.init = main(args);
